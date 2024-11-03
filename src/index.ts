846f8800dae2017ed40c9b85bab7d37964f4342a
Abebe123000 <takashi123000@yahoo.co.jp>
2024-11-03 16:57:56 +0900

    kv example

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from 'hono'
+
+type Bindings = {
+  massage418_session_kv: KVNamespace;
+}
+const app = new Hono<{Bindings: Bindings}>()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/kv/put', async (c) => {
+  await c.env.massage418_session_kv.put('hoge', 'hogeValue')
+  return c.text('put value!')
+})
+
+app.get('kv/get', async (c) => {
+  const hogeValue = await c.env.massage418_session_kv.get('hoge')
+  return c.text(hogeValue ?? 'no value!')
+})
+
+app.get('kv/delete', async (c) => {
+  await c.env.massage418_session_kv.delete('hoge')
+  return c.text('delete value!')
+})
+
+export default app
